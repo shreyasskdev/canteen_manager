9d8f60eb4056c55fa027f70cda90cfc085f28072
Shreyas sk <shreyassk.dev@gmail.com>
2025-02-13 10:09:02 +0530

    feat: Homepage added

--- queue_app/urls.py
+++ queue_app/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 app_name = "queue_app"  # <--- ADD THIS LINE
 
+# urlpatterns = [
+#     path('', views.home, name='home'),
+# ]
 urlpatterns = [
     path('', views.home, name='home'),
 ]
