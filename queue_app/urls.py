533210f219b7327c92b6d1876fa49df39a04dbc6
Shreyas sk <shreyassk.dev@gmail.com>
2025-02-13 11:09:33 +0530

    queue implemented

--- queue_app/urls.py
+++ queue_app/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = "queue_app"  # <--- ADD THIS LINE
+app_name = 'queue_app'
 
-# urlpatterns = [
-#     path('', views.home, name='home'),
-# ]
 urlpatterns = [
     path('', views.home, name='home'),
-    path('order-confirmation/<int:order_id>/', views.order_confirmation, name='order_confirmation'),
+    path('queue/', views.queue_view, name='queue'),
+        path('pop/', views.pop_queue, name='pop_queue'),
 ]
